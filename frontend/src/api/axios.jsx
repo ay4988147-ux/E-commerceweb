c16a9eb03362db04bc3fd17c2c7926273257e340
anil yadav <ay4988147@gmail.com>
2026-09-20 10:58:38 +0530

    axios updataion 2

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: " https://e-commerceweb-4-mkab.onrender.com/api",
+    baseURL: "https://e-commerceweb-4-mkab.onrender.com/api",
 });
 
 export default api;
